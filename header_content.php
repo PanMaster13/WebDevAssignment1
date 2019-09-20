8c02c1859076e004d3cfcf7b83d8b20b0b70fe5a
PanMaster13 <36907215+PanMaster13@users.noreply.github.com>
2019-09-20 11:07:40 +0800

    Header Content
    Created header content for all pages. Edit header_content.php to change its content

--- header_content.php
+++ header_content.php
@@ -1,4 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-</html>
+<p><img src="images/logo.png" alt="Kuching Marathon Logo"></p>
+<h1>Kuching Marathon Official</h1>
+<p>Let's run to our heart's content & for charity</p>
--- rating_input.php
+++ rating_input.php
@@ -1,4 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+<head>
+	<title>Rating Input</title>
+	<meta charset="utf-8">
+	<meta name="author" content="Jason">
+</head>
 
+<body>
+	<header>
+		<?php
+			include "header_content.php";
+		?>
+	</header>
+</body>
 </html>
