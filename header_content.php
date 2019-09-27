998898a8e99b8975e35ed8217f71cb3eef809122
PanMaster13 <36907215+PanMaster13@users.noreply.github.com>
2019-09-27 11:16:11 +0800

    Made self call validation

--- header_content.php
+++ header_content.php
@@ -1,3 +1,7 @@
-<p><img src="images/logo.png" alt="Kuching Marathon Logo"></p>
-<h1>Kuching Marathon Official</h1>
-<p>Let's run to our heart's content & for charity</p>
+<?php
+	
+echo "<p><img src='images/logo.png' alt='Kuching Marathon Logo'></p>";
+echo "<h1>Kuching Marathon Official</h1>";
+echo "<p>Let's run to our heart's content & for charity</p>";
+
+?>
--- rating_landing.php
+++ rating_landing.php
@@ -1,7 +1,3 @@
-<?php
-session_start();
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -17,24 +13,83 @@ session_start();
 		?>
 	</header>
 	
+	<?php
+		if (isset($_GET["submit_button"]))
+		{
+			$message = "";
+			$firstName = $_GET["fName"];
+			$lastName = $_GET["lName"];
+			$regID = $_GET["regID"];
+			$email = $_GET["email"];
+			
+			// Checking First Name Value
+			if (empty($firstName))
+			{
+				$message .= "First Name is required.<br/>";
+			}
+			else
+			{
+				if (!preg_match("/^[a-zA-Z ]*$/", $firstName))
+				{
+					$message .= "First Name is incorrect.<br/>";
+				}
+			}
+			
+			// Checking Last Name Value
+			if (empty($lastName))
+			{
+				$message .= "Last Name is required.<br/>";
+			}
+			else
+			{
+				if (!preg_match("/^[a-zA-Z ]*$/", $LastName))
+				{
+					$message .= "Last Name is incorrect.<br/>";
+				}
+			}
+			
+			// Checking Email Value
+			if (empty($email))
+			{
+				$message .= "Email address is required.<br/>";
+			}
+			else
+			{
+				// FILTER_VALIDATE_EMAIL is a PHP built-in constant to vaildate email
+				if (!filter_var($email, FILTER_VALIDATE_EMAIL))
+				{
+				$message .= "Email address is incorrect.<br/>";
+				}
+			}
+			
+			if ($message != "")
+			{
+				header("Location: rating_landing.php");
+			}
+			else if($message == "")
+			{
+				header("Location: rating_input.php");
+			}
+		}
+		else
+		{
+			$message = "";
+		}
+	?>
+	
 	<article>
 		<form action="validation1.php" method="get">
 		<p>First Name: <input type="text" name="fName"></p>
 		<p>Last Name: <input type="text" name="lName"></p>
 		<p>Registration ID: <input type="text" name="regID"></p>
 		<p>Email address: <input type="text" name="email"></p>
-		<p><input type="submit"></p>
+		<p><input type="submit" name="submit_button"></p>
 		</form>
 	</article>
 	
 	<footer>
 		<?php
-			if (!isset($_SESSION["errorMsg"]))
-			{
-				$_SESSION["errorMsg"] = "";
-			}
-			
-			echo $_SESSION["errorMsg"];
+			echo $message;
 		?>
 	</footer>
 </body>
