bf09488c80bf09243091ec8a787c01908170c733
PanMaster13 <36907215+PanMaster13@users.noreply.github.com>
2019-10-03 22:27:58 +0800

    captcha completed. but half-assed version
    something wrong with the captcha.php file, especially the imagettftext() function.

--- rating_landing.php
+++ rating_landing.php
@@ -19,6 +19,17 @@
 	</header>
 	
 	<?php
+		function init_Captcha()
+		{
+			$num1=rand(1,9);
+			$num2=rand(1,9);
+			$total=$num1+$num2;
+			
+			$_SESSION["total"] = $total;
+
+			$_SESSION["display"] = "$num1 + $num2 = ";
+		}
+		
 		if (isset($_GET["submit_button"]))
 		{
 			$message = "";
@@ -26,6 +37,7 @@
 			$lastName = $_GET["lName"];
 			$regID = $_GET["regID"];
 			$email = $_GET["email"];
+			$captcha = $_GET["captcha"];
 			
 			// Checking Registration ID Value
 			if (empty($regID))
@@ -90,6 +102,36 @@
 				}
 			}
 			
+			// Checking Captcha Value
+			if (empty($captcha))
+			{
+				$message .= "Captcha must be filled.<br/>";
+				
+				$num1=rand(1,9);
+				$num2=rand(1,9);
+				$total=$num1+$num2;
+			
+				$_SESSION["total"] = $total;
+
+				$display = "$num1 + $num2 = ";
+			}
+			else
+			{
+				if(!($captcha == $_SESSION["total"]))
+				{
+					$message .= "Captcha is incorrect. Please try the captcha again.<br/>";
+					
+					// Resets captcha value
+					$num1=rand(1,9);
+					$num2=rand(1,9);
+					$total=$num1+$num2;
+			
+					$_SESSION["total"] = $total;
+
+					$display = "$num1 + $num2 = ";
+				}
+			}
+			
 			// Only move to next page if error message is empty
 			if($message == "")
 			{
@@ -103,17 +145,30 @@
 		else
 		{
 			$message = "";
+			$num1=rand(1,9);
+			$num2=rand(1,9);
+			$total=$num1+$num2;
+			
+			$_SESSION["total"] = $total;
+
+			$display = "$num1 + $num2 = ";
 		}
 	?>
 	
 	<article>
 		<form action="rating_landing.php" method="get">
-		<p>First Name*: <input type="text" name="fName" required="required"></p>
-		<p>Last Name*: <input type="text" name="lName" required="required"></p>
-		<p>Registration ID*: <input type="text" name="regID" placeholder="KM-xx-yyyy"></p>
-		<p>Email address*: <input type="text" name="email"></p>
+		<p>First Name*: <input type="text" name="fName" required="required"/></p>
+		<p>Last Name*: <input type="text" name="lName" required="required"/></p>
+		<p>Registration ID*: <input type="text" name="regID" placeholder="KM-xx-yyyy"/></p>
+		<p>Email address*: <input type="text" name="email"/></p>
 		
-		<p><input class="button" type="reset" name="reset_button"><input class="button" type="submit" name="submit_button"></p>
+		<p>Please enter the answer for the captcha below:</p>
+		<?php
+			echo "<div id='captcha'>", $display, "</div>";
+		?>
+		<p><input type="text" name="captcha" /></p>
+		
+		<p><input class="button" type="reset" name="reset_button"/><input class="button" type="submit" name="submit_button"/></p>
 		</form>
 	</article>
 	
