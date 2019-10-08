97df96dad6a5b53c8dc0b5b570f7fc8982dcfb63
PanMaster13 <36907215+PanMaster13@users.noreply.github.com>
2019-10-08 19:42:14 +0800

    Included student information comments
    As demanded by the submission requirements.

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,3 +1,10 @@
+/* 		Student Name: Jason Ang Chia Wuen
+		Student ID: 100087252
+		Unit Name: Web Application Development
+		Unit Code: COS30020
+		Tutorial Group: 02 
+*/
+
 // Clears any inputs on the form
 function clearForm()
 {
